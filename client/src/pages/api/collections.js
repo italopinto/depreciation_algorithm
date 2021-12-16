c5eb0c3c90ff4bd8a563ba677a3395a6157b79a0
Italo <italosp00@gmail.com>
2021-12-16 14:51:40 -0300

    Configuraçao do client para deploy em produçao

--- client/src/pages/api/collections.js
+++ client/src/pages/api/collections.js
@@ -5,8 +5,10 @@ export default async function handler(req, res) {
 
   const PORT = process.env.PORT || 5000;
   const HOST = process.env.HOST || "0.0.0.0";
+  const URL = NODE_ENV ? HOST : HOST + ":" + PORT;
+  const http = NODE_ENV ? 'https' : 'http';
 
-  const data = await axios.get(`http://${HOST}:${PORT}/api/v1/${collName}`);
+  const data = await axios.get(`${http}://${URL}/api/v1/${collName}`);
   console.log(data.data);
   res.status(200).send(data.data);
 }
